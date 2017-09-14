3dda835952d655d680dba526cc25e0dbaf66065d
Paul Kuznecov <mail@paul-kuznecov.de>
2017-09-14 15:18:05 +1000

    Add place search

--- app/src/main/java/au/edu/unimelb/mc/trippal/NewTripActivity.java
+++ app/src/main/java/au/edu/unimelb/mc/trippal/NewTripActivity.java
@@ -1,23 +1,72 @@
 package au.edu.unimelb.mc.trippal;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocomplete;
 
 import au.edu.unimelb.mc.trippal.camera.FaceTrackerActivity;
-import au.edu.unimelb.mc.trippal.recommendations.Recommendations;
 
 public class NewTripActivity extends AppCompatActivity {
+    public int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+    public EditText destinationText;
+    public TextInputLayout destinationLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_trip);
+        destinationText = (EditText) findViewById(R.id.destination);
+        destinationLayout = (TextInputLayout) findViewById(R.id.destinationWrapper);
+
+        destinationLayout.clearFocus();
+        destinationText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent pintent =
+                            new PlaceAutocomplete.IntentBuilder(PlaceAutocomplete.MODE_FULLSCREEN)
+                                    .zzlf(destinationText.getText().toString())
+                                    .build(NewTripActivity.this);
+                    startActivityForResult(pintent, PLACE_AUTOCOMPLETE_REQUEST_CODE);
+                } catch (GooglePlayServicesRepairableException e) {
+                    // TODO: Handle the error.
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    // TODO: Handle the error.
+                }
+            }
+        });
     }
 
     public void startNewTrip(View view) {
         Intent intent = new Intent(this, FaceTrackerActivity.class);
         startActivity(intent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                Place place = PlaceAutocomplete.getPlace(this, data);
+                Log.i("APP", "Place: " + place.getName());
+                destinationText.setText(place.getName());
+                destinationText.clearFocus();
+            } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                Status status = PlaceAutocomplete.getStatus(this, data);
+                // TODO: Handle the error.
+                Log.i("APP", status.getStatusMessage());
+            } else if (resultCode == RESULT_CANCELED) {
+                // The user canceled the operation.
+            }
+        }
     }
 }
